bdda989c11944b28f4f679b497c719b6e24c9af1
Pauli Matila <pauli.matila@outlook.com>
2020-04-01 09:51:02 +0300

    Add project files.

---
+++ DataHandler-harjoitus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataHandler_harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
